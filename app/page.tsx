8d42aa230756104a69b80803354612de6b414916
Paolo Escobar <paolo.escobar@mail.udp.cl>
2024-07-25 01:26:50 +0000

    segunda parte

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,116 @@
 "use client";
-import React, { useState } from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-const photos = [
-    {
-        id: 1,
-        url: "/image/photo-1.avif",
-        title: "Golden Gate Bridge 1",
-    },
-    {
-        id: 2,
-        url: "/image/photo-2.avif",
-        title: "Golden Gate Bridge 2",
-    },
-    {
-        id: 3,
-        url: "/image/photo-3.avif",
-        title: "Golden Gate Bridge 3",
-    },
-    {
-        id: 4,
-        url: "/image/photo-4.avif",
-        title: "Golden Gate Bridge 4",
-    },
-    {
-        id: 5,
-        url: "/image/photo-5.avif",
-        title: "Golden Gate Bridge 5",
-    },
-    {
-        id: 6,
-        url: "/image/photo-6.avif",
-        title: "Golden Gate Bridge 6",
-    },
-];
+import React, { useState, useEffect } from "react";
+import { scrapingPhotos } from "../scraping";
+import { useWindowSize } from "@uidotdev/usehooks";
 
-const HomePage: React.FC = () => {
+interface Author {
+    username: string | undefined;
+    profileImageUrl: string | undefined;
+    twitter: string | undefined;
+}
+
+interface Photo {
+    id: number;
+    url: string;
+    profileUrl: string;
+    tags: string[];
+    author: any;
+}
+interface HomePageProps {
+    photos: Photo[];
+}
+const HomePage: React.FC<HomePageProps> = () => {
+    const [photos, setPhotos] = useState<Photo[]>([]);
     const [hoveredPhoto, setHoveredPhoto] = useState<number | null>(null);
+    const size = useWindowSize();
+
+    useEffect(() => {
+        async function getData() {
+            const photos: Photo[] = await scrapingPhotos();
+            setPhotos(photos);
+        }
+        getData();
+    }, []);
+
+    const getColumns = (photos: Photo[]) => {
+        const columns = size.width >= 1024 ? 3 : size.width >= 768 ? 2 : 1;
+
+        const columnArrays: Photo[][] = Array.from(
+            { length: columns },
+            () => []
+        );
+        photos.forEach((photo, index) => {
+            columnArrays[index % columns].push(photo);
+        });
+        return columnArrays;
+    };
+    const columnsPhotos = getColumns(photos);
     return (
         <div>
             <h2 className="text-3xl font-bold mb-6">Golden Gate</h2>
             <div className="columns-1 md:columns-2 lg:columns-3 gap-6">
-                {photos.map((photo) => (
-                    <div
-                        key={photo.id}
-                        className="relative bg-white shadow-md rounded-lg overflow-hidden mb-6"
-                        onMouseEnter={() => setHoveredPhoto(photo.id)}
-                        onMouseLeave={() => setHoveredPhoto(null)}
-                    >
-                        <div className="relative w-full">
-                            <img
-                                src={photo.url}
-                                alt={photo.title}
-                                className="w-full h-auto"
-                            />
-                            {hoveredPhoto === photo.id && (
-                                <>
-                                    <div className="absolute top-6 left-6">
-                                        <img
-                                            src="/icon.png"
-                                            alt="icon"
-                                            className="w-16 h-16"
-                                        />
-                                    </div>
-                                    <span className="absolute bottom-2 left-2 flex items-center bg-gray-800 bg-opacity-40 p-2 rounded">
-                                        <svg className="inline-block h-10 w-10 rounded-full">
-                                            <circle
-                                                cx="5"
-                                                cy="5"
-                                                r="50"
-                                                fill="blue"
-                                            />
-                                        </svg>
-                                        <div className="ml-2 text-white">
-                                            Texto de ejemplo
-                                        </div>
-                                    </span>
-                                </>
-                            )}
-                        </div>
-                        <div className="p-4 bg-white flex gap-2 flex-wrap">
-                            <a
-                                href="#"
-                                className="px-3 py-1 bg-gray-200 text-gray-700 rounded-lg text-xs font-medium hover:bg-gray-300"
+                {columnsPhotos.map((column, columnIndex) => (
+                    <div key={columnIndex} className="space-y-6">
+                        {column.map((photo) => (
+                            <div
+                                key={photo.id}
+                                className="relative bg-white shadow-md rounded-lg overflow-hidden mb-6"
+                                onMouseEnter={() => setHoveredPhoto(photo.id)}
+                                onMouseLeave={() => setHoveredPhoto(null)}
                             >
-                                tag 1
-                            </a>
-                            <a
-                                href="#"
-                                className="px-3 py-1 bg-gray-200 text-gray-700 rounded-lg text-xs font-medium hover:bg-gray-300"
-                            >
-                                tag 2
-                            </a>
-                            <a
-                                href="#"
-                                className="px-3 py-1 bg-gray-200 text-gray-700 rounded-lg text-xs font-medium hover:bg-gray-300"
-                            >
-                                tag 3
-                            </a>
-                        </div>
+                                <div className="relative w-full">
+                                    <img
+                                        src={photo.url}
+                                        alt="nada"
+                                        className="w-full h-auto"
+                                    />
+                                    {hoveredPhoto === photo.id && (
+                                        <>
+                                            <div className="absolute top-6 left-6">
+                                                <img
+                                                    src="/icon.png"
+                                                    alt="icon"
+                                                    className="w-16 h-16"
+                                                />
+                                            </div>
+                                            <span className="absolute bottom-2 left-2 flex items-center bg-gray-800 bg-opacity-40 p-2 rounded">
+                                                {photo.author
+                                                    .profileImageUrl && (
+                                                    <img
+                                                        src={
+                                                            photo.author
+                                                                .profileImageUrl
+                                                        }
+                                                        alt={
+                                                            photo.author
+                                                                .username
+                                                        }
+                                                        className="inline-block h-10 w-10 rounded-full"
+                                                    />
+                                                )}
+                                                <div className="ml-2 text-white">
+                                                    {photo.author.username}
+                                                </div>
+                                                <div className="ml-2 text-white">
+                                                    {photo.author.twitter}
+                                                </div>
+                                            </span>
+                                        </>
+                                    )}
+                                </div>
+                                <div className="p-4 bg-white flex gap-2 flex-wrap">
+                                    {photo.tags.map((tag, index) => (
+                                        <a
+                                            href="#"
+                                            key={index}
+                                            className="px-3 py-1 bg-gray-200 text-gray-700 rounded-lg text-xs font-medium hover:bg-gray-300"
+                                        >
+                                            {tag}
+                                        </a>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 ))}
             </div>
---
+++ scraping.ts
@@ -0,0 +1,111 @@
+"use server";
+import * as cheerio from "cheerio";
+
+interface Author {
+    username: string | undefined;
+    profileImageUrl: string | undefined;
+    twitter: string | undefined;
+}
+
+interface Photo {
+    id: number;
+    url: string;
+    profileUrl: string;
+    tags: string[];
+    author: any;
+}
+
+const UNSPLASH_URL = "https://unsplash.com/s/photos/golden-gate";
+
+export async function scrapingPhotos(numberPhotos = 19): Promise<Photo[]> {
+    try {
+        const response = await fetch(UNSPLASH_URL, { cache: "no-store" });
+        const data = await response.text();
+        const $ = cheerio.load(data);
+        const photos: Photo[] = [];
+
+        const authorProfiles: Array<Promise<Author | null>> = [];
+
+        $("figure").each((index, element) => {
+            if (index >= numberPhotos) {
+                return false;
+            }
+            const imgElement = $(element).find("img[srcset]").last();
+            const srcSet = imgElement.attr("srcset");
+            const tags: string[] = [];
+
+            $(element)
+                .find('div > a[class*="VyS40"]')
+                .each((_, tagElement) => {
+                    const tag = $(tagElement).text().trim();
+                    if (tag) {
+                        tags.push(tag);
+                    }
+                });
+
+            const imgUrl = srcSet
+                ?.split(",")
+                .map((item) => item.trim().split(" ")[0])
+                .pop();
+            const profileLinkElement = $(element)
+                .find("a.OQSsT, a.BkSVh")
+                .first();
+            const profileUrl = profileLinkElement.attr("href");
+
+            if (imgUrl && profileUrl) {
+                const authorProfilePromise = scrapeAuthorProfile(
+                    `https://unsplash.com${profileUrl}`
+                );
+                authorProfiles.push(authorProfilePromise);
+
+                photos.push({
+                    id: index + 1,
+                    url: imgUrl,
+                    profileUrl: `https://unsplash.com${profileUrl}`,
+                    tags: tags,
+                    author: null,
+                });
+            }
+        });
+
+        // Resolver todas las promesas de los autores
+        const resolvedAuthors = await Promise.all(authorProfiles);
+
+        // Asignar los datos de los autores resueltos a las fotos correspondientes
+        for (let i = 0; i < photos.length; i++) {
+            photos[i].author = resolvedAuthors[i];
+        }
+
+        return photos;
+    } catch (error) {
+        console.error("Error al extraer datos de Unsplash:", error);
+        return [];
+    }
+}
+
+async function scrapeAuthorProfile(profileUrl: string) {
+    try {
+        const response = await fetch(profileUrl);
+        const data = await response.text();
+        const $ = cheerio.load(data);
+
+        let username = $('meta[property="og:title"]').attr("content");
+        if (username) {
+            username = username.split("|")[0].trim();
+        }
+        const profileImageUrl = $("img.Lg6wf.eqnCe").attr("src");
+        const twitter = $('meta[name="twitter:creator"]').attr("content");
+
+        return {
+            username,
+            profileImageUrl,
+            twitter,
+        };
+    } catch (error) {
+        console.error(
+            `Error al extraer datos del perfil: ${profileUrl}`,
+            error
+        );
+        return null;
+    }
+}
